6b15e8b9331c5f1d21fc7a990dee2983fb0e6ef0
Kiana Hobbs <kianah@gengar.cs.uchicago.edu>
2021-03-16 02:39:10 -0500

    Need to fix with concurrency and phantom matrix

--- scripts/learning_algo.py
+++ scripts/learning_algo.py
@@ -15,11 +15,12 @@ import time
 import math
 import random
 import numpy as np
-from robodog.msg import UserCommand, Reward, ActionStatus, Action
+from robodog.msg import UserCommand, Reward, ActionStatus, Action, LearningMatrix
 from numpy.random import random_sample 
+from copy import deepcopy
 
-commands = ['roll','shake','come','follow','find','fetch']
-colors = ['red','green','blue']
+# commands = ['roll','shake','come','follow','find','fetch']
+# colors = ['red','green','blue']
 
 def draw_random_action(choices, probabilities):
     """ Return a random action from the set choices with the specified probabilities
@@ -27,10 +28,10 @@ def draw_random_action(choices, probabilities):
         probabilities: the probability of selecting each element in choices represented as a list
     """
     episilon = [0.9,0.1]
-    random = np.random.choice([True, False], episilon)
+    random = np.random.choice([True, False], p= episilon)
     # randomly select action
-    if random:
-        return np.random.choice([choices])
+    if not random:
+        return np.random.choice(10)
 
     # Use Q Matrix to select new action
     for p in probabilities:
@@ -38,8 +39,14 @@ def draw_random_action(choices, probabilities):
     values = np.array(range(len(choices)))
     probs = np.array(probabilities)
     bins = np.add.accumulate(probs)
-    inds = values[np.digitize(random_sample(1), bins)]
-    action = choices[int(inds[0])]
+    print(values)
+    print(probs)
+    print(bins)
+    inds = values[np.digitize(random_sample(10), bins)]
+    samples = []
+    for i in inds:
+        samples.append(deepcopy(choices[int(i)]))
+    action = samples[0]
     return action
 
 
@@ -53,45 +60,51 @@ class LearningAlgo(object):
         rospy.Subscriber('/robodog/user_cmd', UserCommand, self.get_command)
         rospy.Subscriber('/robodog/action_reward', Reward, self.get_reward)
         self.action_pub = rospy.Publisher('robodog/action', Action, queue_size=10)
-
+        self.matrix_pub = rospy.Publisher('robodog/learning_matrix', LearningMatrix, queue_size=10)
+        rospy.Subscriber('/robodo/action_status', ActionStatus,self.get_status)
+        self.action_status = "Idle"
         self.command = None
-        self.curr_action = None
+        self.selected_action = None
         self.reward = None
+        self.processed_action = None
 
         self.action_matrix = [[] for x in range(10)]
         for i in range(10):
-            self.action_matrix[i] = [0 for x in range(10)]
-        
+            self.action_matrix[i] = [1/10 for x in range(10)]
+        self.matrix_pub.publish(self.action_matrix)
+
         self.initialized = True
 
+    def get_status(self, data):
+        self.action_status = data.status
 
     def get_reward(self, data):
         self.reward = data.reward 
+        # print(self.action_status)
+        if self.reward and self.action_status == "Complete":
+            # print("Received Reward")
+            # print(self.reward)
+            self.update_action_probs()
 
     def update_action_probs(self):
         alpha = 1
         gamma = 0.5
-        # epsilon = 0.9
 
         # get value
-        current_val = self.action_matrix[self.command][self.curr_action]
+        current_val = self.action_matrix[self.processed_action][self.selected_action]
         
         # get max value of all actions for state2
-        max_action = max(self.action_matrix[self.command])
+        max_action = max(self.action_matrix[self.processed_action])
 
         # update q matrix for state1 & action_t
-        self.action_matrix[self.command][self.curr_action]  += \
-            int(alpha * (self.reward.reward + gamma * max_action  - current_val))
-        
-
-        # Normalize probabilities
-        # prob_sum = 0.0
-        # for prob in self.action_matrix[self.command]:
-        #     prob_sum += prob
-
-        # for i in range(10):
-        #     self.action_matrix[self.command][i] =  self.action_matrix[self.command][i]/prob_sum
-
+        self.action_matrix[self.processed_action][self.selected_action]  += \
+            int(alpha * (self.reward/10.0 + gamma * max_action  - current_val))
+        print("Updating Action Matrix")
+        print(self.action_matrix)
+        matrix = LearningMatrix()
+        matrix.matrix = self.action_matrix
+        self.matrix_pub.publish(matrix)
+ 
 
     def get_command(self, data):
         if not self.initialized:
@@ -132,29 +145,18 @@ class LearningAlgo(object):
             return 
         
         # Subscribe to topic that sends the processed command and convert command to numeric
-        given_action = self.process_command
+        self.processed_action = self.process_command()
+        print(self.processed_action)
         # Select an action using the action probability matrix
         actions = [i for i in range(10)]
-        self.curr_action = draw_random_action(actions, self.action_matrix[given_action])
+        self.selected_action = draw_random_action(actions, self.action_matrix[self.processed_action])
+        
         # Publish the action
         action = Action()
-        action.action = self.curr_action
+        action.action = self.selected_action
         self.action_pub.publish(action)
-
-    
-    def initialized_action_probability_matrix(self):
-        
-        # Populate State Space
-        state_space = []
-        for command in range(10):
-            for function in range(10):
-                    state_space.append((command, function))
-        
-        # Set action probability
-        for s1 in state_space:
-            for s2 in state_space:
-                self.action_matrix[s1][s2] = 1/10
-                return
+        print(self.selected_action)
+        print("action complete\n")
 
 
     def run(self):
--- scripts/phantom_movement.py
+++ scripts/phantom_movement.py
@@ -9,14 +9,14 @@ from q_learning_project.msg import RobotMoveDBToBlock
 import time
 
 from tf.transformations import quaternion_from_euler, euler_from_quaternion
-from robodog.msg import UserCommand, Reward, ActionStatus, Action
+from robodog.msg import UserCommand, Reward, ActionStatus, Action, LearningMatrix
 import random
 
 
 class PhantomDogMovement(object):
 
     def __init__(self):
-
+        self.initialized = False
         # initialize this node
         rospy.init_node('dog_phantom_movement')
 
@@ -27,9 +27,13 @@ class PhantomDogMovement(object):
         self.action_pub = rospy.Publisher('robodog/action', Action, queue_size=10)
 
         rospy.Subscriber('/robodog/action_status', ActionStatus, self.get_action_status)
-        self.action_status =None
+        self.action_status_pub = rospy.Publisher('/robodog/action_status', ActionStatus, queue_size=10)
+        self.action_status ="Idle"
 
         self.reward_pub = rospy.Publisher('/robodog/action_reward', Reward, queue_size=10)
+
+        rospy.Subscriber('/robodog/learning_matrix', LearningMatrix, self.get_matrix)
+        self.learning_matrix =None
         # information about the robot action to take
         self.dog_action_queue = []
 
@@ -49,11 +53,13 @@ class PhantomDogMovement(object):
             "roll"
         ]
 
+        self.initialized = True
         self.execute_robot_action()
 
 
     def execute_robot_action(self):
-
+        if not self.initialized:
+            return 
         time.sleep(0.5)
 
         if (len(self.actions_seq) > 0):
@@ -64,21 +70,31 @@ class PhantomDogMovement(object):
             command = UserCommand()
             command.command = robot_command_to_take
             self.command_pub.publish(command)
-    
-            # while not self.action_status:
-            #     print("waiting for action completion")
-
+            rospy.sleep(1)
             # Give arbitary reward
-            reward = random.uniform(0, 1)
+            self.action_status_pub.publish(ActionStatus(status ="Complete"))
+            while self.action_status != "Complete":
+                print("waiting for action completion")
+            print("Action marked as completed!")
+            reward = random.randint(0, 10)
             self.reward_pub.publish(reward)
+            rospy.sleep(1)
+            curr_matrix = self.learning_matrix
+            while curr_matrix == self.learning_matrix:
+                print("Waiting for matrix to update")
+            print(self.learning_matrix)
             # reset the flag and the action in the queue
             self.actions_seq.pop(0)
-
+            self.action_status_pub.publish(ActionStatus(status = "Idle"))
+            rospy.sleep(1)
             self.execute_robot_action()
 
 
     def get_action_status(self, data):
-        self.action_status = data.complete
+        self.action_status = data.status
+
+    def get_matrix(self, data):
+        self.learning_matrix = data.matrix
 
     def run(self):
         rospy.spin()
